42d7bdcc1c785a3f16abd69506997bd3dbe54835
Souvik Mishra <mishra.souvik911@gmail.com>
2023-09-04 17:47:07 +0530

    made blog section visually appealing with 2 grids

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -34,27 +34,76 @@ const blogs = [
     readingTime: '2 min read',
     imageUrl: './stay-tuned.webp',
   },
+  {
+    id: 4,
+    title: 'Blog 4',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    tags: ['tag1', 'tag2', 'tag3'],
+    date: '2021-07-01',
+    slug: 'blog-4',
+    readingTime: '2 min read',
+    imageUrl: './stay-tuned.webp',
+  },
+  {
+    id: 5,
+    title: 'Blog 5',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    tags: ['tag1', 'tag2', 'tag3'],
+    date: '2021-07-01',
+    slug: 'blog-5',
+    readingTime: '2 min read',
+    imageUrl: './stay-tuned.webp',
+  },
+  {
+    id: 6,
+    title: 'Blog 6',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    tags: ['tag1', 'tag2', 'tag3'],
+    date: '2021-07-01',
+    slug: 'blog-6',
+    readingTime: '2 min read',
+    imageUrl: './stay-tuned.webp',
+  },
 ]
-
-const blogPosts = blogs.map((blog) => {
-  return (
-    <BlogPost
-      key={blog.id}
-      title={blog.title}
-      content={blog.content}
-      tags={blog.tags}
-      date={blog.date}
-      slug={blog.slug}
-      readingTime={blog.readingTime}
-      imageUrl={blog.imageUrl}
-    />
-  )
-})
 
 export default function Page() {
   return (
-    <div className="my-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
-      {blogPosts}
-    </div>
+    <>
+      <div className="my-4 grid grid-cols-1 gap-4 lg:grid-cols-[8fr_4fr]">
+        {blogs.slice(0, 2).map((blog) => {
+          return (
+            <BlogPost
+              key={blog.id}
+              title={blog.title}
+              content={blog.content}
+              tags={blog.tags}
+              date={blog.date}
+              slug={blog.slug}
+              readingTime={blog.readingTime}
+              imageUrl={blog.imageUrl}
+            />
+          )
+        })}
+      </div>
+      <div className="my-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        {blogs.slice(2).map((blog) => {
+          return (
+            <BlogPost
+              key={blog.id}
+              title={blog.title}
+              content={blog.content}
+              tags={blog.tags}
+              date={blog.date}
+              slug={blog.slug}
+              readingTime={blog.readingTime}
+              imageUrl={blog.imageUrl}
+            />
+          )
+        })}
+      </div>
+    </>
   )
 }
