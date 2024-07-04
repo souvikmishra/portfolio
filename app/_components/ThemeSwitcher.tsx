a6ce1610cc1d1da06eea64f03887509f0721a7f3
Souvik Mishra <mishra.souvik911@gmail.com>
2024-07-04 15:36:38 +0530

    added icons for theme switcher

--- app/_components/ThemeSwitcher.tsx
+++ app/_components/ThemeSwitcher.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
-
+import { FaMoon } from 'react-icons/fa'
+import { FiSun } from 'react-icons/fi'
 const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
@@ -15,17 +16,24 @@ const ThemeSwitcher = () => {
     return null
   }
 
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
   return (
-    <select
-      value={theme}
-      onChange={(e) => {
-        setTheme(e.target.value)
-      }}
+    <button
+      className="group flex items-center rounded-md p-2 hover:bg-zinc-300 focus:bg-zinc-300 dark:hover:bg-zinc-800 dark:focus:bg-zinc-800"
+      onClick={() => toggleTheme()}
     >
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+      {theme === 'dark' ? (
+        <FiSun className="duration-100 group-hover:rotate-45" size={'20px'} />
+      ) : (
+        <FaMoon
+          className="-rotate-[20deg] duration-100 group-hover:rotate-[15deg]"
+          size={'20px'}
+        />
+      )}
+    </button>
   )
 }
 
